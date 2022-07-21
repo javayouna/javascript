6240c5f6524f10ecf18c45896ae84a8ce53f02ea
javayouna <106093094+javayouna@users.noreply.github.com>
2022-07-21 18:17:00 +0900

    Add files via upload

---
+++ js13.js
@@ -0,0 +1,22 @@
+/*
+function api(){
+	this.map=function(){
+		var way ="신촌역 2번 출구";
+		
+	}
+	this.login=function(mid){
+		var msg;
+		if(mid=="hong"){
+			msg="해당 고객을 확인하였습니다. 오유나";
+		}
+		
+		else{
+			msg="error 오유나";
+		}
+		return msg;
+	}
+	
+	
+}
+var kakao2 = new api();
+*/
